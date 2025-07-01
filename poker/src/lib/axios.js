c7c5e9d3328842de42763505bf260f5cded73082
Noozey <121247095+Noozey@users.noreply.github.com>
2025-07-01 23:58:02 +0545

    Update axios.js

--- poker/src/lib/axios.js
+++ poker/src/lib/axios.js
@@ -3,7 +3,7 @@ import supabase from "../../supabaseClient";
 
 let token = localStorage.getItem(`sb-${supabase.supabaseKey}-auth-token`);
 export const api = axios.create({
-  baseURL: "http://localhost:3000", // Change to your API base URL
+  baseURL: "poker-production-71d8.up.railway.app", // Change to your API base URL
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
